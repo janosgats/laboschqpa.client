be662dca8b848f20db852caaf56c87cf5a81b857
Dzsani <gatsjanos@gmail.com>
2021-05-10 23:27:26 +0200

    logInAs reloads currentUser context

--- src/pages/admin/users.tsx
+++ src/pages/admin/users.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import {NextPage} from "next";
-import React from "react";
+import React, {useContext} from "react";
 import useEndpoint from "~/hooks/useEndpoint";
 import {UserInfo} from "~/model/UserInfo";
 import {CircularProgress} from "@material-ui/core";
@@ -8,25 +8,32 @@ import AdminNavBar from "~/components/nav/AdminNavBar";
 import {teamRoleData} from "~/enums/TeamRole";
 import callJsonEndpoint from "~/utils/api/callJsonEndpoint";
 import EventBus from "~/utils/EventBus";
+import {CurrentUserContext} from "~/context/CurrentUserProvider";
 
-function logInAsUser(userId: number) {
-    callJsonEndpoint({
-        conf: {
-            url: '/api/up/server/api/admin/users/logInAsUser',
-            method: "post",
-            params: {
-                userAccId: userId
-            }
-        }
-    }).then(() => EventBus.notifyInfo(`Logged in as user ${userId}`))
-}
 
 const Index: NextPage = () => {
+    const currentUser = useContext(CurrentUserContext);
+
     const usedEndpoint = useEndpoint<UserInfo[]>({
         conf: {
             url: "/api/up/server/api/user/listAll"
         }
     });
+
+    function logInAsUser(userId: number) {
+        callJsonEndpoint({
+            conf: {
+                url: '/api/up/server/api/admin/users/logInAsUser',
+                method: "post",
+                params: {
+                    userAccId: userId
+                }
+            }
+        }).then(() => {
+            currentUser.reload();
+            EventBus.notifyInfo(`Logged in as user ${userId}`);
+        })
+    }
 
     return (
         <>
