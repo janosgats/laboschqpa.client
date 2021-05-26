b60c9c8e2b8912651f22f986ab6bce6571e44c59
Dzsani <gatsjanos@gmail.com>
2021-05-27 00:40:09 +0200

    Small UX tweak

--- src/components/email/AddNewEmailAddressDialog.tsx
+++ src/components/email/AddNewEmailAddressDialog.tsx
@@ -19,6 +19,7 @@ export const AddNewEmailAddressDialog: FC<Props> = (props) => {
     useEffect(() => {
         if (props.isOpen) {
             setEmail('');
+            setApiCallPending(false);
         }
     }, [props.isOpen])
 
