c858cb2ab816cdffcc303f19fc30dab0d5f75948
elod.sandor <elod.sandor@accretivetg.com>
2021-07-27 01:57:11 +0200

    Navigation bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,38 @@
-import '../styles/globals.css'
 import React from "react";
 import EventDisplayContainer from "~/components/eventDisplay/EventDisplayContainer";
 import CurrentUserProvider from "~/context/CurrentUserProvider";
 import NavBar from "~/components/nav/NavBar";
 import Head from "next/head";
+import { createMuiTheme, Paper, ThemeProvider } from '@material-ui/core';
+import { useState } from 'react';
 
-function MyApp({Component, pageProps}): JSX.Element {
+function MyApp({ Component, pageProps }): JSX.Element {
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const theme = createMuiTheme({
+        palette: {
+            type: darkMode ? "dark" : "light"
+                }
+    });
+
     return (
         <>
-            <Head>
-                <title>Qpa</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <EventDisplayContainer/>
-            <CurrentUserProvider>
-                <NavBar/>
-                <Component {...pageProps} />
-            </CurrentUserProvider>
+            <ThemeProvider theme={theme} >
+                <Paper>
+                    <Head>
+                        <title>Qpa</title>
+                        <link rel="icon" href="/favicon.ico" />
+                        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                    </Head>
+                    <EventDisplayContainer />
+                    <CurrentUserProvider>
+                        <NavBar darkMode={darkMode} setDarkMode={setDarkMode}>
+                            <Component {...pageProps} />  
+                        </NavBar>
+                    </CurrentUserProvider>
+                </Paper>
+            </ThemeProvider>
         </>
     );
 }
