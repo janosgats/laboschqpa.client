459bba89118c201339ffb729df19a5997d2f9ff6
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-09-14 20:21:53 +0200

    Create File info modal

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,11 +1,13 @@
 import {Box, CircularProgress} from '@material-ui/core';
 import React, {ReactElement} from 'react';
 
-interface Props {}
+interface Props {
+    minWidth?: any;
+}
 
-export default function Spinner({}: Props): ReactElement {
+export default function Spinner({minWidth}: Props): ReactElement {
     return (
-        <Box width="100%" pt={1} justifyContent="center" display="flex">
+        <Box width="100%" pt={1} minWidth={minWidth} justifyContent="center" display="flex">
             <CircularProgress />
         </Box>
     );
