5e98ebbf0efda9484ae1c3b3fb56abac7015d700
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-09-14 20:34:16 +0200

    Improve FileUploaderDialog

--- src/components/file/FileUploaderDialog.tsx
+++ src/components/file/FileUploaderDialog.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useState } from "react";
-import FileToUpload, { UploadedFileType } from "~/model/usergeneratedcontent/FileToUpload";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
+import {Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography} from '@material-ui/core';
+import {Publish} from '@material-ui/icons';
+import React, {FC, useState} from 'react';
+import FileToUpload, {UploadedFileType} from '~/model/usergeneratedcontent/FileToUpload';
 
 interface Props {
     uploadedFileType: UploadedFileType;
@@ -13,48 +13,40 @@ interface Props {
 const FileUploaderDialog: FC<Props> = (props) => {
     const [file, setFile] = useState<File>();
 
+    console.log(file);
+
     const onFileChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
         if (e.target?.files?.length) {
             setFile(e.target.files[0]);
         }
-    }
+    };
 
     const onUploadButtonClicked: React.MouseEventHandler<HTMLInputElement> = async (e) => {
         props.onUploadInitiation(new FileToUpload(file, props.uploadedFileType));
-    }
+    };
 
     return (
-        <Dialog 
-            open={props.isOpen} 
-            onClose={props.onClose}
-            maxWidth="sm"
-            fullWidth
-            >
-            <DialogTitle >Upload file</DialogTitle>
+        <Dialog open={props.isOpen} onClose={props.onClose} maxWidth="sm" fullWidth>
+            <DialogTitle>Fájl feltöltés</DialogTitle>
             <DialogContent>
-                <Button
-                    variant="outlined"
-                    component="label"
-                    startIcon={<SearchIcon />}
-                >
-                    Search file
-                    <input type="file"
-                        hidden
-                        onChange={onFileChange}
-                        {
-                        ...(props.uploadedFileType === UploadedFileType.IMAGE
-                            ? { accept: "image/*" }
-                            : {})
-                        }
-                    />
-                </Button>
+                <Box mx="auto" width="fit-content">
+                    <Button variant="outlined" component="label" startIcon={<Publish />} fullWidth>
+                        <Typography>{file?.name || 'SEARCH FILE'}</Typography>
+                        <input
+                            type="file"
+                            hidden
+                            onChange={onFileChange}
+                            {...(props.uploadedFileType === UploadedFileType.IMAGE ? {accept: 'image/*'} : {})}
+                        />
+                    </Button>
+                </Box>
             </DialogContent>
             <DialogActions>
-                <Button onClick={props.onClose} color="secondary">
-                    Cancel
+                <Button onClick={props.onClose} color="secondary" variant="outlined">
+                    Mégsem
                 </Button>
-                <Button onClick={onUploadButtonClicked} color="primary" disabled={!file}>
-                    Upload
+                <Button onClick={onUploadButtonClicked} color="primary" variant="contained" disabled={!file}>
+                    Feltöltés
                 </Button>
             </DialogActions>
         </Dialog>
