07e37304de8e52d1c2b8727bb9c1828d897e329f
elod.sandor <elod.sandor@accretivetg.com>
2021-09-21 22:40:38 +0200

    teamsTable

---
+++ src/components/team/styles/TeamStyle.ts
@@ -0,0 +1,14 @@
+export const  style ={
+
+    tableContainer:{
+        borderRadius: "16px",
+        padding: "16px"
+    },
+
+
+    tableRow:{
+        '&:hover': {
+            cursor: "pointer",
+        },
+    },
+}
--- src/pages/teams/index.tsx
+++ src/pages/teams/index.tsx
@@ -1,71 +1,100 @@
-import {Button, Container, Table, TableCell, TableHead, TableRow, useTheme} from '@material-ui/core';
-import {NextPage} from 'next';
+import { Container, createStyles, Grid, Icon, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Theme, Typography, useTheme } from '@material-ui/core';
+import { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
 import NotTeamMemberBanner from '~/components/banner/NotTeamMemberBanner';
-import MyPaper from '~/components/mui/MyPaper';
 import Spinner from '~/components/Spinner';
 import useEndpoint from '~/hooks/useEndpoint';
+import { style } from '~/components/team/styles/TeamStyle';
 
 interface TeamWithScore {
-    id: number;
-    name: string;
-    archived: boolean;
-    score: number;
+  id: number;
+  name: string;
+  archived: boolean;
+  score: number;
 }
 
+const useStyles = makeStyles((theme: Theme) => createStyles(style))
+const baseUrl = "https://laboschqpa-public.s3.pl-waw.scw.cloud/static/frontend/qpatrophy/";
+const svgEndPoints = ["gold.svg", "silver.svg", "bronze.svg"];
+
 const Index: NextPage = () => {
-    const theme = useTheme();
 
-    const usedEndpoint = useEndpoint<TeamWithScore[]>({
-        conf: {
-            url: '/api/up/server/api/team/listActiveTeamsWithScores',
-        },
-    });
+  const classes = useStyles()
 
+  const usedEndpoint = useEndpoint<TeamWithScore[]>({
+    conf: {
+      url: '/api/up/server/api/team/listActiveTeamsWithScores',
+    },
+  });
+
+  const separatedPoints = (score: number) => score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+  const loadQpaIcon = (index: number) => {
+    console.log("Calling : " + baseUrl + svgEndPoints[index])
     return (
-        <Container maxWidth="lg">
-            <Head>
-                <title>Teams</title>
-            </Head>
+      <>
+        <img
+          src={baseUrl + svgEndPoints[index]}
+          width={125 - index * 25}
+          height={125 - index * 25}
+        />
+      </>
+    )
+  }
 
-            <NotTeamMemberBanner />
+  return (
+    <Container maxWidth="lg">
+      <Head>
+        <title>Teams</title>
+      </Head>
 
-            {usedEndpoint.pending && <Spinner />}
-            {usedEndpoint.failed && <p>Couldn't load teams :'(</p>}
-            {usedEndpoint.data && (
-                <MyPaper>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>#</TableCell>
-                                <TableCell>Team Name</TableCell>
-                                <TableCell>Score</TableCell>
-                                <TableCell></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        {usedEndpoint.data.map((team, index) => {
-                            return (
-                                <TableRow>
-                                    <TableCell>{team.id}</TableCell>
-                                    <TableCell>{team.name}</TableCell>
-                                    <TableCell>{team.score}</TableCell>
-                                    <TableCell>
-                                        <Link href={`/teams/team/${team.name}?id=${team.id}`}>
-                                            <Button variant="contained" color="primary">
-                                                Show Team
-                                            </Button>
-                                        </Link>
-                                    </TableCell>
-                                </TableRow>
-                            );
-                        })}
-                    </Table>
-                </MyPaper>
-            )}
-        </Container>
-    );
+      <NotTeamMemberBanner />
+
+      {usedEndpoint.pending && <Spinner />}
+      {usedEndpoint.failed && <p>Couldn't load teams :'(</p>}
+      {usedEndpoint.data && (
+        <TableContainer component={Paper}
+          className={classes.tableContainer}
+        >
+          <Table size="medium"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell variant="head" align="center"><Typography variant="h4">Helyezés</Typography></TableCell>
+                <TableCell variant="head" align="center"><Typography variant="h4">Csapat név</Typography></TableCell>
+                <TableCell variant="head" align="center"><Typography variant="h4">Pontszám</Typography></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {usedEndpoint.data.map((team, index) => {
+                return (
+                  <Link href={`/teams/team/${team.name}?id=${team.id}`}>
+                    <TableRow
+                      key={index}
+                      hover
+                      className={classes.tableRow}
+                    >
+                      <TableCell align="center">
+                          {index < 3 ? loadQpaIcon(index) :
+                            <Typography variant="h5">
+                              {index + 1}.
+                            </Typography>
+                          }
+                      </TableCell>
+                      <TableCell align="center"><Typography variant="h5">{team.name}</Typography></TableCell>
+                      <TableCell align="center"><Typography variant="h5">{separatedPoints(team.score)}</Typography></TableCell>
+                    </TableRow>
+                  </Link>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </Container>
+  );
 };
 
 export default Index;
