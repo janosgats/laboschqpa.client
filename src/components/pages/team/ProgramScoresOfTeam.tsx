e0def20aa0799759203775abe054675b3e2a1882
elod.sandor <elod.sandor@accretivetg.com>
2021-09-22 01:24:27 +0200

    ProgramScorsOfTea UI

--- src/components/pages/team/ProgramScoresOfTeam.tsx
+++ src/components/pages/team/ProgramScoresOfTeam.tsx
@@ -1,11 +1,15 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import useEndpoint from "~/hooks/useEndpoint";
-import {Program} from "~/model/usergeneratedcontent/Program";
+import { Program } from "~/model/usergeneratedcontent/Program";
 import Spinner from "~/components/Spinner";
-
+import { Grid, makeStyles, Paper, Table, TableCell, TableContainer, TableHead, TableRow, Theme, Typography } from "@material-ui/core";
+import { getStyles } from '~/components/team/styles/TeamStyle';
 interface Props {
     teamId: number;
 }
+
+const useStyles = makeStyles((theme: Theme) => getStyles(theme))
+
 
 const ProgramScoresOfTeam: FC<Props> = (props) => {
     const usedEndpoint = useEndpoint<Program[]>({
@@ -19,25 +23,57 @@ const ProgramScoresOfTeam: FC<Props> = (props) => {
         deps: [props.teamId],
     });
 
-    return (
-        <div>
-            <h3>Pontok:</h3>
+    const separatedPoints = (score: number) => score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-            {usedEndpoint.pending && <Spinner/>}
+    const classes= useStyles();
+
+    return (
+        <>
+
+            {usedEndpoint.pending && <Spinner />}
 
             {usedEndpoint.failed && <p>Couldn't load program scores :'(</p>}
 
             {usedEndpoint.succeeded && (
-                <ul>
-                    {usedEndpoint.data
-                        .map((program, index) => {
-                            return (
-                                <li>{program.title}: {program.teamScore}</li>
-                            );
-                        })}
-                </ul>
+                <>
+                    <Grid
+                        container
+                        justify="space-between"
+                    >
+                        <Typography variant="h4">Elért pontszám: </Typography>
+                        <Typography variant="h4">
+                            {separatedPoints(usedEndpoint.data.reduce((sum, program) => sum + program.teamScore, 0))}
+                        </Typography>
+                    </Grid>
+
+
+                    <TableContainer 
+                        component={Paper}
+                        className={classes.tableContainer}
+                        >
+                        <Table size="medium">
+                            <TableHead>
+                                <TableRow >
+                                    <TableCell variant="head"><Typography ><b>Program</b></Typography></TableCell>
+                                    <TableCell variant="head" align="right"><Typography ><b>Kapott pont</b></Typography></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            {usedEndpoint.data
+                                .map((program, index) => {
+                                    return (
+                                        <TableRow
+                                            hover
+                                        >
+                                            <TableCell><Typography> {program.title} </Typography> </TableCell>
+                                            <TableCell align="right"><Typography> {separatedPoints(program.teamScore)} </Typography> </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                        </Table>
+                    </TableContainer>
+                </>
             )}
-        </div>
+        </>
     );
 };
 
