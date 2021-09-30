9a0e0d54a105ac3fcebb9118bb368602c60a33aa
Dzsani <gatsjanos@gmail.com>
2021-09-30 02:46:36 +0200

    Added GlobalCache of speedDrinkingUsers

--- src/components/fetchableDisplay/ui/SpeedDrinkingEditor.tsx
+++ src/components/fetchableDisplay/ui/SpeedDrinkingEditor.tsx
@@ -2,7 +2,6 @@ import {Button, createStyles, Dialog, DialogContent, makeStyles, TextField, Them
 import {Autocomplete} from '@material-ui/lab';
 import React, {FC} from 'react';
 import SpeedDrinkingCategorySelector from '~/components/selector/SpeedDrinkingCategorySelector';
-import Spinner from '~/components/Spinner';
 import {SpeedDrinkingCategory} from '~/enums/SpeedDrinkingCategory';
 import useEndpoint from '~/hooks/useEndpoint';
 import {UserInfo} from '~/model/UserInfo';
@@ -10,6 +9,7 @@ import {dialogStyles} from '~/styles/dialog-styles';
 import {isValidNumber} from '~/utils/CommonValidators';
 import UserNameFormatter from '~/utils/UserNameFormatter';
 import {filterByNormalizedLowercaseWorldSplit} from "~/utils/filterByNormalizedLowercaseWorldSplit";
+import useGlobalCache, {GlobalCache, GlobalCacheKey} from "~/hooks/useGlobalCache";
 
 interface Props {
     isOpen: boolean;
@@ -45,90 +45,99 @@ function getSelectorOptionLabelForUser(userInfo: UserInfo): string {
 const SpeedDrinkingEditor: FC<Props> = (props) => {
     const classes = useStyles();
 
+    const usersCache: GlobalCache<UserInfo[]> = useGlobalCache<UserInfo[]>(GlobalCacheKey.SPEED_DRINKING_USERS);
     const usedEndpointUsers = useEndpoint<UserInfo[]>({
         conf: {
             url: '/api/up/server/api/user/listAllEnabledWithTeamName',
         },
+        onSuccess: resp => {
+            usersCache.setData(resp.data);
+        }
     });
-    const fetchedUsers = usedEndpointUsers.data;
+
+    function getRefreshUsersCacheButtonText() {
+        if (usedEndpointUsers.pending) {
+            return 'Refreshing cache...';
+        }
+
+        let text = 'Refresh users cache';
+        if (usedEndpointUsers.failed) {
+            text += ' (last failed)';
+        }
+        return text;
+    }
 
     return (
         <Dialog open={props.isOpen} onClose={() => props.onCancel()}>
             <DialogContent className={classes.dialogContainer}>
-                <>
-                    {usedEndpointUsers.pending && <Spinner />}
+                <h2 className={classes.dialogTitle}>Record Time</h2>
 
-                    {usedEndpointUsers.failed && (
-                        <>
-                            <p>Couldn't load users :'(</p>
-                            <button
-                                onClick={() => {
-                                    usedEndpointUsers.reloadEndpoint();
-                                }}
-                            >
-                                Retry
-                            </button>
-                        </>
-                    )}
+                <Button onClick={() => usedEndpointUsers.reloadEndpoint()}
+                        disabled={usedEndpointUsers.pending}
+                        color="secondary"
+                        variant="outlined">
+                    {getRefreshUsersCacheButtonText()}
+                </Button>
 
-                    {fetchedUsers && (
-                        <>
-                            <h2 className={classes.dialogTitle}>Record Time</h2>
-                            <Autocomplete
-                                style={{width: '350px', padding: '10px 0'}}
-                                options={fetchedUsers}
-                                getOptionLabel={(userInfo) => getSelectorOptionLabelForUser(userInfo)}
-                                renderInput={(params) => <TextField {...params} label="Drinker:" variant="outlined" />}
-                                value={fetchedUsers.filter((u) => u.userId === props.drinkerUserId)[0]}
-                                onChange={(e, val: UserInfo) => {
-                                    if (val && isValidNumber(val.userId)) {
-                                        props.setDrinkerUserId(val.userId);
-                                    }
-                                }}
-                                filterOptions={filterByNormalizedLowercaseWorldSplit}
-                            />
+                {usersCache.isSet && (
+                    <>
+                        <Autocomplete
+                            style={{width: '350px', padding: '10px 0'}}
+                            options={usersCache.data}
+                            getOptionLabel={(userInfo) => getSelectorOptionLabelForUser(userInfo)}
+                            renderInput={(params) => <TextField {...params} label="Drinker:" variant="outlined"/>}
+                            value={usersCache.data.filter((u) => u.userId === props.drinkerUserId)[0]}
+                            onChange={(e, val: UserInfo) => {
+                                if (val && isValidNumber(val.userId)) {
+                                    props.setDrinkerUserId(val.userId);
+                                }
+                            }}
+                            filterOptions={filterByNormalizedLowercaseWorldSplit}
+                        />
 
-                            <SpeedDrinkingCategorySelector value={props.category} onChange={props.setCategory} />
+                        <SpeedDrinkingCategorySelector value={props.category} onChange={props.setCategory}/>
 
-                            <TextField
-                                style={{margin: '20px 0 10px 0'}}
-                                variant="outlined"
-                                type="number"
-                                inputProps={{step: 0.01}}
-                                value={props.time}
-                                onChange={(e) => props.setTime(Number.parseFloat(e.target.value))}
-                                label="Time"
-                            />
+                        <TextField
+                            style={{margin: '20px 0 10px 0'}}
+                            variant="outlined"
+                            type="number"
+                            inputProps={{step: 0.01}}
+                            value={props.time}
+                            onChange={(e) => props.setTime(Number.parseFloat(e.target.value))}
+                            label="Time"
+                        />
 
-                            <TextField
-                                style={{margin: '10px 0'}}
-                                variant="outlined"
-                                value={props.note}
-                                onChange={(e) => props.setNote(e.target.value)}
-                                label="Note"
-                            />
+                        <TextField
+                            style={{margin: '10px 0'}}
+                            variant="outlined"
+                            value={props.note}
+                            onChange={(e) => props.setNote(e.target.value)}
+                            label="Note"
+                        />
 
-                            {props.isCreatingNew && (
-                                <Button variant="contained" onClick={props.onSave} disabled={props.isApiCallPending}>
-                                    Létrehoz
+                        {props.isCreatingNew && (
+                            <Button variant="contained" onClick={props.onSave} disabled={props.isApiCallPending}>
+                                Létrehoz
+                            </Button>
+                        )}
+                        {!props.isCreatingNew && (
+                            <>
+                                <Button variant="contained" onClick={props.onSave}
+                                        disabled={props.isApiCallPending}>
+                                    Szerkesztés
                                 </Button>
-                            )}
-                            {!props.isCreatingNew && (
-                                <>
-                                    <Button variant="contained" onClick={props.onSave} disabled={props.isApiCallPending}>
-                                        Szerkesztés
-                                    </Button>
-                                    <Button variant="contained" onClick={props.onCancel} disabled={props.isApiCallPending}>
-                                        Mégse
-                                    </Button>
-                                    <Button variant="contained" onClick={props.onDelete} disabled={props.isApiCallPending}>
-                                        Törlés
-                                    </Button>
-                                </>
-                            )}
-                        </>
-                    )}
-                </>
+                                <Button variant="contained" onClick={props.onCancel}
+                                        disabled={props.isApiCallPending}>
+                                    Mégse
+                                </Button>
+                                <Button variant="contained" onClick={props.onDelete}
+                                        disabled={props.isApiCallPending}>
+                                    Törlés
+                                </Button>
+                            </>
+                        )}
+                    </>
+                )}
             </DialogContent>
         </Dialog>
     );
