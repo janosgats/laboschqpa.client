3c4e43ab1524eae2aecf23a818fbe2e01c932da4
Dzsani <gatsjanos@gmail.com>
2021-09-30 03:51:36 +0200

    Autocomplete modals on submissions page

--- src/pages/submissions/index.tsx
+++ src/pages/submissions/index.tsx
@@ -5,13 +5,13 @@ import {
     Grid,
     InputLabel,
     makeStyles,
-    Select,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableHead,
     TableRow,
+    TextField,
     Theme,
     Typography,
 } from '@material-ui/core';
@@ -22,15 +22,24 @@ import NotAcceptedByEmailBanner from '~/components/banner/NotAcceptedByEmailBann
 import MyPaper from '~/components/mui/MyPaper';
 import SubmissionsPanel from '~/components/panel/SubmissionsPanel';
 import Spinner from '~/components/Spinner';
-import {objectiveTypeData} from '~/enums/ObjectiveType';
 import useEndpoint from '~/hooks/useEndpoint';
 import {TeamInfo} from '~/model/Team';
 import {Objective} from '~/model/usergeneratedcontent/Objective';
 import {isValidNumber} from '~/utils/CommonValidators';
 import DateTimeFormatter from '~/utils/DateTimeFormatter';
 import {styles} from '../../styles/submissionStyles/submissionsPage.styles';
+import {filterByNormalizedLowercaseWorldSplit} from "~/utils/filterByNormalizedLowercaseWorldSplit";
+import {Autocomplete} from "@material-ui/lab";
 
-const NOT_FILTERED = 'not_filtered';
+const NOT_FILTERED_TEAM_INFO: TeamInfo = {
+    id: -99,
+    name: 'Nincs szűrés',
+    archived: false,
+};
+const NOT_FILTERED_OBJECTIVE: Partial<Objective> = {
+    id: -99,
+    title: 'Nincs szűrés',
+};
 
 const useStyles = makeStyles((theme: Theme) => createStyles(styles));
 
@@ -60,10 +69,10 @@ const Index: NextPage = () => {
                 <title>Submissions</title>
             </Head>
 
-            <NotAcceptedByEmailBanner />
-            <br />
+            <NotAcceptedByEmailBanner/>
+            <br/>
 
-            {(usedEndpointObjectives.pending || usedEndpointTeams.pending) && <Spinner />}
+            {(usedEndpointObjectives.pending || usedEndpointTeams.pending) && <Spinner/>}
 
             {(usedEndpointObjectives.failed || usedEndpointTeams.failed) && (
                 <>
@@ -87,33 +96,24 @@ const Index: NextPage = () => {
                                 <InputLabel id="objective-filter" className={classes.formControlBox}>
                                     Szűrés feladatra
                                 </InputLabel>
-                                <Select
-                                    fullWidth
-                                    defaultValue={NOT_FILTERED}
-                                    native
-                                    labelId="objective-filter"
-                                    value={filteredObjectiveId !== null ? filteredObjectiveId : ''}
-                                    onChange={(e) => {
-                                        const val = e.target.value;
-                                        if (isValidNumber(val.toString())) {
-                                            setFilteredObjectiveId(Number.parseInt(e.target.value.toString()));
-                                        }
-                                        if (val === NOT_FILTERED) {
+
+                                <Autocomplete
+                                    options={[NOT_FILTERED_OBJECTIVE, ...fetchedObjectives]}
+                                    getOptionLabel={(objective: Objective) => objective.title}
+                                    renderInput={(params) => <TextField {...params} label="Feladat" variant="outlined"/>}
+                                    value={filteredObjectiveId !== null ? fetchedObjectives.filter((t) => t.id === filteredObjectiveId)[0] : NOT_FILTERED_OBJECTIVE}
+                                    onChange={(e, val: Objective) => {
+                                        if (val === NOT_FILTERED_OBJECTIVE) {
                                             setFilteredObjectiveId(null);
+                                            return;
+                                        }
+                                        if (val && isValidNumber(val.id)) {
+                                            setFilteredObjectiveId(Number.parseInt(val.id as any));
                                         }
                                     }}
-                                >
-                                    <option value={NOT_FILTERED}>Nincs</option>
-                                    {fetchedObjectives
-                                        .filter((item) => item.submittable)
-                                        .map((objective) => {
-                                            return (
-                                                <option key={objective.id} value={objective.id}>
-                                                    {`${objectiveTypeData[objective.objectiveType].shortDisplayName} > ${objective.title}`}
-                                                </option>
-                                            );
-                                        })}
-                                </Select>
+                                    filterOptions={filterByNormalizedLowercaseWorldSplit}
+                                />
+
                             </FormControl>
                         </Grid>
                         <Grid item>
@@ -121,7 +121,7 @@ const Index: NextPage = () => {
                                 <Typography variant="h4">Szűrés feladatra</Typography>
                                 <TableContainer
                                     component={MyPaper}
-                                    style={{maxWidth:"calc(100vw - 30vw)", overflow:"auto"}}
+                                    style={{maxWidth: "calc(100vw - 30vw)", overflow: "auto"}}
                                 >
                                     <Table size="small">
                                         <TableHead>
@@ -139,25 +139,25 @@ const Index: NextPage = () => {
                                         </TableHead>
                                         <TableBody>
                                             {fetchedObjectives &&
-                                                fetchedObjectives
-                                                    .filter((item) => item.submittable)
-                                                    .map((objective: Objective, index: number) => {
-                                                        return (
-                                                            <TableRow
-                                                                key={index}
-                                                                onClick={() =>
-                                                                    setFilteredObjectiveId((f) => (f == objective.id ? null : objective.id))
-                                                                }
-                                                                hover
-                                                                className={classes.tableRow}
-                                                                selected={objective.id === filteredObjectiveId}
-                                                            >
-                                                                <TableCell>{objective.title}</TableCell>
-                                                                <TableCell>{objective.submittable ? 'Beadható' : 'Lejárt'}</TableCell>
-                                                                <TableCell>{DateTimeFormatter.toFullBasic(objective.deadline)}</TableCell>
-                                                            </TableRow>
-                                                        );
-                                                    })}
+                                            fetchedObjectives
+                                                .filter((item) => item.submittable)
+                                                .map((objective: Objective, index: number) => {
+                                                    return (
+                                                        <TableRow
+                                                            key={index}
+                                                            onClick={() =>
+                                                                setFilteredObjectiveId((f) => (f == objective.id ? null : objective.id))
+                                                            }
+                                                            hover
+                                                            className={classes.tableRow}
+                                                            selected={objective.id === filteredObjectiveId}
+                                                        >
+                                                            <TableCell>{objective.title}</TableCell>
+                                                            <TableCell>{objective.submittable ? 'Beadható' : 'Lejárt'}</TableCell>
+                                                            <TableCell>{DateTimeFormatter.toFullBasic(objective.deadline)}</TableCell>
+                                                        </TableRow>
+                                                    );
+                                                })}
                                         </TableBody>
                                     </Table>
                                 </TableContainer>
@@ -171,35 +171,29 @@ const Index: NextPage = () => {
                                 <InputLabel id="team-filter" className={classes.formControlBox}>
                                     Szűrés csapatra
                                 </InputLabel>
-                                <Select
-                                    fullWidth
-                                    defaultValue={NOT_FILTERED}
-                                    native
-                                    labelId="team-filter"
-                                    value={filteredTeamId !== null ? filteredTeamId : ''}
-                                    onChange={(e) => {
-                                        const val = e.target.value;
-                                        if (isValidNumber(val.toString())) {
-                                            setFilteredTeamId(Number.parseInt(e.target.value.toString()));
-                                        }
-                                        if (val === NOT_FILTERED) {
+
+                                <Autocomplete
+                                    options={[NOT_FILTERED_TEAM_INFO, ...fetchedTeams]}
+                                    getOptionLabel={(team: TeamInfo) => team.name + (team.archived ? ' (archive)' : '')}
+                                    renderInput={(params) => <TextField {...params} label="Csapat" variant="outlined"/>}
+                                    value={filteredTeamId !== null ? fetchedTeams.filter((t) => t.id === filteredTeamId)[0] : NOT_FILTERED_TEAM_INFO}
+                                    onChange={(e, val: TeamInfo) => {
+                                        if (val === NOT_FILTERED_TEAM_INFO) {
                                             setFilteredTeamId(null);
+                                            return;
+                                        }
+                                        if (val && isValidNumber(val.id)) {
+                                            setFilteredTeamId(Number.parseInt(val.id as any));
                                         }
                                     }}
-                                >
-                                    <option value={NOT_FILTERED}>Nincs</option>
-                                    {fetchedTeams.map((team) => {
-                                        return (
-                                            <option key={team.id} value={team.id}>
-                                                {team.name + (team.archived ? ' (archive)' : '')}
-                                            </option>
-                                        );
-                                    })}
-                                </Select>
+                                    filterOptions={filterByNormalizedLowercaseWorldSplit}
+                                />
+
                             </FormControl>
                         </Grid>
                         <Grid item>
-                            <SubmissionsPanel filteredObjectiveId={filteredObjectiveId} filteredTeamId={filteredTeamId} />
+                            <SubmissionsPanel filteredObjectiveId={filteredObjectiveId}
+                                              filteredTeamId={filteredTeamId}/>
                         </Grid>
                     </Grid>
                 </Grid>
