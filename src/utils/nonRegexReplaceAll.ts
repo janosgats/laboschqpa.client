1db2813de450050e1739af28e14c27b18fd8ab33
János Gats <gatsjanos@gmail.com>
2021-09-30 21:18:35 +0200

    hotfix

--- src/utils/nonRegexReplaceAll.ts
+++ src/utils/nonRegexReplaceAll.ts
@@ -8,7 +8,8 @@ export default function nonRegexReplaceAll(subject: string, searchValue: string,
     if (!isValidNonEmptyString(subject)) {
         return '';
     }
-
+    //TODO: this has a bug that breaks speeddrinking user slection. Fix it later. now just "reverting" it with a quick github edit
+return subject.replaceAll(searchValue,replaceValue);
     let processed = subject;
 
     while (processed.includes(searchValue)) {
